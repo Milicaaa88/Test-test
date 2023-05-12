a62d8646371b760acf07ee5d38ccf09b62f73ee6
Milicaaa88 <milica.petrovic.se@gmail.com>
2023-05-12 09:26:51 +0200

    jjj

---
+++ test/procitajRecensije.js
@@ -0,0 +1,43 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const should = require('chai').should();
+
+/* As a customer, 
+I would like to read all reviwes from previous customers
+because I want be sure if I want to buy some product */
+
+// Test: Read reviews
+describe("Read product reviews", () => {
+
+    // Testfall
+  context("When I navigate to the product page", () => {
+
+    it("should display all product reviews", async () => {
+      const driver = await new Builder().forBrowser('firefox').build();
+      
+      try {
+        // Gå till butiken
+        await driver.get('https://magento.softwaretestingboard.com/');
+
+        // Search the product
+        await driver.wait(until.elementLocated(By.id('search')), 10000);
+        await driver.findElement(By.id('search')).sendKeys('bag', Key.RETURN);
+        
+        // Wait for the reviews section to load
+        await driver.wait(until.elementLocated(By.css('.action.view')), 10000);
+        await driver.findElement(By.css('.action.view')).click();
+       
+        // Wait for 2 sec
+        await driver.sleep(2000);
+
+        // Find all review comments
+        const reviewComments = await driver.findElements(By.css('.reviews > .review-content'));
+
+
+      } catch (error) {
+        console.log(error);
+      } finally {
+        await driver.quit();
+      }
+    });
+});
+});
